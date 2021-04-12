58e8490919d03984b427744771324f3d5a37f474
Rob Prellwitz <robprellwitz@uwalumni.com>
2021-04-12 12:34:58 -0500

    added webview form for event data from database

--- livinitup/livinitup/Master1.Master.designer.cs
+++ livinitup/livinitup/Master1.Master.designer.cs
@@ -51,39 +51,21 @@ namespace livinitup
         protected global::System.Web.UI.WebControls.ContentPlaceHolder UniqueContent1;
 
         /// <summary>
-        /// lblEvent1 control.
+        /// GridView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblEvent1;
+        protected global::System.Web.UI.WebControls.GridView GridView1;
 
         /// <summary>
-        /// lblEvent2 control.
+        /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblEvent2;
-
-        /// <summary>
-        /// lblEvent3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblEvent3;
-
-        /// <summary>
-        /// lblEvent4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblEvent4;
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
